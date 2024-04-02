987bdec7fc1f8fd9e1ca0fe65bcf68d200094d72
Santosh Barai <barai3022@gmail.com>
2024-04-02 23:34:07 +0600

    pre906i

--- src/utility/JWFTokenHelper.js
+++ src/utility/JWFTokenHelper.js
@@ -1,16 +1,3 @@
-# 906 [Backend] Create JWF Token Helper
-
-### https://www.npmjs.com/package/jose
-
-`npm i jose`
-
-
-JWT_SECRET="123-XYZ-ABC"
-JWT_ISSUER="Localhost"
-JWT_EXPIRATION_TIME="24h"
-
-
-```
 import { SignJWT, jwtVerify } from "jose";
 
 export async function CreateToker(email, id){
@@ -24,14 +11,11 @@ export async function CreateToker(email, id){
         .sign(secret)
 
     return token;
+
 }
-```
 
-
-```
 export async function VerifyToken(token){
     const secret=new TextEncoder().sncode(process.env.JWT_SECRET);
     const decoded = await jwtVerify(token, secret);
     return decoded['payload'];
 }
-```

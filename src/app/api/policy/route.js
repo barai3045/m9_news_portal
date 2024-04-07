f0af06e66fe7e70abc5f98b0fb95eb8b1cfc7a9b
Santosh Barai <barai3022@gmail.com>
2024-04-07 16:36:02 +0600

    pre917

--- src/app/api/policy/route.js
+++ src/app/api/policy/route.js
@@ -1,7 +1,3 @@
-# 917 [API] Socials & Policies 
-
-## Policy
-```
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
@@ -17,21 +13,3 @@ export async function GET(req, res){
         return NextResponse.json({status:"fail", data: e})
     }
 }
-```
-
-##  Socials
-```
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-export async function GET(req, res){
-    try{
-        const prisma = new PrismaClient();
-        const result = await prisma.socials.findMany()
-        return NextResponse.json({status:"success", data: result})
-
-    } catch(e){
-        return NextResponse.json({status:"fail", data: e})
-    }
-}
-```

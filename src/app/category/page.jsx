9b593a00a0f3af77229743d6ccf8cad7b2f9d7fa
Santosh Barai <barai3022@gmail.com>
2024-04-09 21:37:28 +0600

    pre921f

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+    return (
+        <div>
+            
+        </div>
+    );
 };
 
 export default page;

a35593b456632abf00bfba8d5ae0a11c65f69783
Santosh Barai <barai3022@gmail.com>
2024-04-10 13:32:01 +0600

    pre925

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-# 924 [Front End] Working with Plain Layout
+# 926 [Front End] Working with Plain Layout
 
 
-## data facing on plain layout
+## subscribe update
 
-```
-async function getData(){
-    let socials = (await (await fetch(`${process.env.HOST}/api/social`)).json())['data']
-    let categories = (await (await fetch(`${process.env.HOST}/api/category`)).json())['data']
-    return {socials:socials, categories:categories}
-}
-```
+## plain layout updated
+
+## submit button updated
--- src/components/master/Plain-Layout.jsx
+++ src/components/master/Plain-Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppNavBar from './AppNavBar';
 import Footer from './Footer';
+import {Toaster} from 'react-hot-toast'
 
 
 async function getData(){
     let socials = (await (await fetch(`${process.env.HOST}/api/social`)).json())['data']
     let categories = (await (await fetch(`${process.env.HOST}/api/category`)).json())['data']
+    //let categories = [] 
     return {socials:socials, categories:categories}
 }
 
@@ -18,6 +20,7 @@ const PlainLayout = async (props) => {
         <>
             <AppNavBar data={data} />
             {props.children}
+            <Toaster position='bottom-center' />
             <Footer data={data}/>
         </>
     );
